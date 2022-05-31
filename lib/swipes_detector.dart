521320b211d6399bfdc85f9d73a6e01ab667419a
Dmitry <dmitry.maslov.almeza@gmail.com>
2022-05-31 08:59:15 +0300

    Рефакторинг

--- lib/swipes_detector.dart
+++ lib/swipes_detector.dart
@@ -33,25 +33,44 @@ class _SwipesDetectorState extends State<SwipesDetector> {
     globalPosition: const Offset(0, 0),
   );
 
+  _onDragEnd({
+    required bool horizontal,
+    required final DragStartDetails start,
+    required final DragUpdateDetails last,
+    required final DragEndDetails end,
+    final VoidCallback? onBegin,
+    final VoidCallback? onEnd,
+  }) {
+    Offset offset = last.globalPosition - start.globalPosition;
+    double primaryOffset = horizontal ? offset.dx : offset.dy;
+    if (primaryOffset != 0.0) {
+      primaryOffset < 0.0 ? onBegin?.call() : onEnd?.call();
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return GestureDetector(
       onHorizontalDragStart: (details) => startH = details,
       onHorizontalDragUpdate: (details) => lastH = details,
-      onHorizontalDragEnd: (details) {
-        Offset offset = lastH.globalPosition - startH.globalPosition;
-        double primaryOffset = offset.dx;
-        if (primaryOffset > 0.0) return widget.onSwipeRight();
-        if (primaryOffset < 0.0) return widget.onSwipeLeft();
-      },
+      onHorizontalDragEnd: (details) => _onDragEnd(
+        horizontal: true,
+        start: startH,
+        last: lastH,
+        end: details,
+        onBegin: widget.onSwipeLeft,
+        onEnd: widget.onSwipeRight,
+      ),
       onVerticalDragStart: (details) => startV = details,
       onVerticalDragUpdate: (details) => lastV = details,
-      onVerticalDragEnd: (details) {
-        Offset offset = lastV.globalPosition - startV.globalPosition;
-        double primaryOffset = offset.dy;
-        if (primaryOffset > 0.0) return widget.onSwipeDown();
-        if (primaryOffset < 0.0) return widget.onSwipeUp();
-      },
+      onVerticalDragEnd: (details) => _onDragEnd(
+        horizontal: false,
+        start: startV,
+        last: lastV,
+        end: details,
+        onBegin: widget.onSwipeUp,
+        onEnd: widget.onSwipeDown,
+      ),
       child: widget.child,
     );
   }
